98c22b30cafd15075a3133130396086ccebe6430
mtrempoltsev <mt@devexp.ru>
2020-01-23 13:50:16 +0300

    add script to build v8

---
+++ thirdparty/build_v8.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+if test ! -f thirdparty/build_v8.sh ;
+then
+  echo "ERROR: Please run this script from the root folder of v8capi"
+  exit 1
+fi
+
+ROOT_DIR=$(pwd)
+
+cd thirdparty
+
+if test ! -d v8 ;
+then
+    rm -rf .cipd .gclient .gclient_entries
+    ./depot_tools/fetch v8 || exit 1
+fi
+
+cd v8
+
+git checkout 8.1.248 || exit 1
+$ROOT_DIR/thirdparty/depot_tools/gclient sync || exit 1
+
+tools/dev/v8gen.py x64.release.sample || exit 1
+ninja -C out.gn/x64.release.sample v8_monolith || exit 1
+
+if test ! -L libv8_monolith.a ;
+then
+    ln -s out.gn/x64.release.sample/obj/libv8_monolith.a . || exit 1
+fi
+
+cd $ROOT_DIR
+
+exit 0
